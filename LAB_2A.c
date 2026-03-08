3e6103f2d2f56edbdc6ae5d27a275db5f32f2cc0
Md. Samiul Islam Siam <samiul908986@gmail.com>
2026-03-08 21:50:31 +0600

    Finally working properly at 45 MHz

--- LAB_2A.c
+++ LAB_2A.c
@@ -1,6 +1,6 @@
 /* Lab 2A - UART2 Polling TX/RX (No HAL) on STM32F446RE
  * UART2 : PA2=TX , PA3=RX | APB1 Bus | Baud 115200 , 8N1
- * APB1 clock assumed = 45 MHz ( configure PLL accordingly )
+ * APB1 clock assumed = 45 MHz (configure PLL accordingly)
  */
 
 #include <stdint.h>
@@ -9,68 +9,66 @@
 #define APB1_CLK 45000000UL
 #define BAUD_RATE 115200UL
 
-void SystemClock_Config(void) {
-	/* 1. Enable HSE */
-	RCC->CR |= RCC_CR_HSEON;
-	while (!(RCC->CR & RCC_CR_HSERDY));
+void SystemClock_Config(void)
+{
+    /* 1. Enable PWR clock */
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
 
-	/* 2. Enable Power interface clock */
-	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+    /* 2. Voltage scaling (Scale 1 mode) */
+    PWR->CR |= PWR_CR_VOS;
 
-	/* 3. Set Voltage Scale 1 */
-	PWR->CR |= PWR_CR_VOS;
+    /* 3. Enable HSI */
+    RCC->CR |= RCC_CR_HSION;
+    while(!(RCC->CR & RCC_CR_HSIRDY));
 
-	/* 4. Enable Over-Drive mode */
-	PWR->CR |= PWR_CR_ODEN;
-	while (!(PWR->CSR & PWR_CSR_ODRDY));
+    /* 4. Configure FLASH latency and enable caches */
+    FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN;
+    FLASH->ACR &= ~FLASH_ACR_LATENCY;
+    FLASH->ACR |= FLASH_ACR_LATENCY_5WS;
 
-	PWR->CR |= PWR_CR_ODSWEN;
-	while (!(PWR->CSR & PWR_CSR_ODSWRDY));
+    /* 5. Configure PLL
+       HSI = 16 MHz
+       PLLM = 8
+       PLLN = 180
+       PLLP = 2
+       PLLQ = 2
+    */
 
-	/* 5. Configure Flash:
-	 - 5 wait states
-	 - Instruction cache
-	 - Data cache
-	 - Prefetch enable
-	 */
-	FLASH->ACR =
-	FLASH_ACR_LATENCY_5WS |
-	FLASH_ACR_ICEN |
-	FLASH_ACR_DCEN |
-	FLASH_ACR_PRFTEN;
+    RCC->PLLCFGR = 0;
+    RCC->PLLCFGR |= (8 << RCC_PLLCFGR_PLLM_Pos);
+    RCC->PLLCFGR |= (180 << RCC_PLLCFGR_PLLN_Pos);
+    RCC->PLLCFGR |= (0 << RCC_PLLCFGR_PLLP_Pos);   // PLLP = 2
+    RCC->PLLCFGR |= (RCC_PLLCFGR_PLLSRC_HSI);
+    RCC->PLLCFGR |= (2 << RCC_PLLCFGR_PLLQ_Pos);
 
-	/* 6. Configure PLL
-	 HSE = 8 MHz
-	 PLLM = 4
-	 PLLN = 180
-	 PLLP = 2
-	 PLLQ = 15
-	 SYSCLK = 180 MHz
-	 */
-	RCC->PLLCFGR = (4 << RCC_PLLCFGR_PLLM_Pos) 
-			| (180 << RCC_PLLCFGR_PLLN_Pos)
-			| (0 << RCC_PLLCFGR_PLLP_Pos) /* PLLP = /2 */
-			| (15 << RCC_PLLCFGR_PLLQ_Pos) 
-			| RCC_PLLCFGR_PLLSRC_HSE;
+    /* 6. Enable PLL */
+    RCC->CR |= RCC_CR_PLLON;
+    while(!(RCC->CR & RCC_CR_PLLRDY));
 
-	/* 7. Configure prescalers
-	 AHB  = SYSCLK / 1  = 180 MHz
-	 APB1 = AHB / 4     = 45 MHz
-	 APB2 = AHB / 2     = 90 MHz
-	 */
-	RCC->CFGR =
-	RCC_CFGR_HPRE_DIV1  |
-	RCC_CFGR_PPRE1_DIV4 |
-	RCC_CFGR_PPRE2_DIV2;
+    /* 7. Enable OverDrive mode */
+    PWR->CR |= PWR_CR_ODEN;
+    while(!(PWR->CSR & PWR_CSR_ODRDY));
 
-	/* 8. Enable PLL */
-	RCC->CR |= RCC_CR_PLLON;
-	while (!(RCC->CR & RCC_CR_PLLRDY));
+    PWR->CR |= PWR_CR_ODSWEN;
+    while(!(PWR->CSR & PWR_CSR_ODSWRDY));
 
-	/* 9. Select PLL as system clock */
-	RCC->CFGR |= RCC_CFGR_SW_PLL;
-	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+    /* 8. Configure Bus Prescalers
+       AHB = SYSCLK /1
+       APB1 = HCLK /4
+       APB2 = HCLK /2
+    */
+
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+
+    /* 9. Select PLL as system clock */
+    RCC->CFGR &= ~RCC_CFGR_SW;
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+    while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
 }
+
 
 void USART2_Init(void) {
 	/* 1. Enable clocks */
@@ -87,12 +85,15 @@ void USART2_Init(void) {
 	GPIOA->OTYPER &= ~((1UL << 2) | (1UL << 3)); /* Push - Pull */
 	GPIOA->OSPEEDR |= ((3UL << 4) | (3UL << 6)); /* Very High */
 
-	/* 3. Baud rate */
-	uint32_t usartdiv = APB1_CLK / BAUD_RATE;
-	uint32_t mantissa = usartdiv;
-	uint32_t fraction = ((APB1_CLK % BAUD_RATE) * 16) / BAUD_RATE;
+	/* 3. Baud rate calculation
+	   fCK = 45 MHz
+	   Baud = 115200
+	   USARTDIV = 24.414 (fCK/(16*Baud))
+	   Mantissa = 24
+	   Fraction ≈ 7
+	*/
 
-	USART2->BRR = (mantissa << 4) | (fraction & 0xF);
+	USART2->BRR = (24 << 4) | 7;   // 0x187
 
 	/* 4. Enable USART , transmitter , receiver */
 	USART2->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
@@ -117,8 +118,8 @@ int main(void) {
 	SystemClock_Config();
 	USART2_Init();
 
-	USART2_SendString(" STM32F446RE UART Polling Demo \r\n");
-	USART2_SendString(" Type a character -- it will be echoed :\r\n");
+	USART2_SendString("STM32F446RE UART Polling Demo\r\n");
+	USART2_SendString("Type a character -- it will be echoed:\r\n");
 
 	while (1) {
 		char c = USART2_RecvChar(); /* blocking receive */
