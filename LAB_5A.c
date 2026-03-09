590db22c709f8cf815df874b1e77683044ccd8a2
Md. Samiul Islam Siam <samiul908986@gmail.com>
2026-03-09 11:00:47 +0600

    Modified to make similar with HAL code
    Commented out unused code and added comments for clarity.

--- LAB_5A.c
+++ LAB_5A.c
@@ -1,6 +1,7 @@
-/* Lab 5A - UART2 DMA TX (Stream6 Ch4) + Circular RX (Stream5 Ch4)
+/* Lab 5A - UART2 DMA TX (Stream6 Ch4) + Circular RX (Stream5 Ch4) [Comment out]
  * No HAL -- direct DMA register configuration
  */
+// Folloowing code is for DMA RX, similar as in HAL code
 
 #include <stdint.h>
 #include <stm32f446xx.h>
@@ -85,7 +86,7 @@ void DMA1_Init(void) {
 
 	DMA1_Stream6->PAR = (uint32_t) &USART2->DR;
 	DMA1_Stream6->CR = (4UL << DMA_SxCR_CHSEL_Pos) /* Channel 4 */
-	| DMA_SxCR_DIR_0 /* Mem -> Periph */
+	| DMA_SxCR_DIR_0 /* Memory -> Peripheral */
 	| DMA_SxCR_MINC /* Memory increment */
 	| DMA_SxCR_TCIE; /* TC interrupt */
 
@@ -97,12 +98,24 @@ void DMA1_Init(void) {
 	DMA1_Stream5->PAR = (uint32_t) &USART2->DR;
 	DMA1_Stream5->M0AR = (uint32_t) rxBuf;
 	DMA1_Stream5->NDTR = RX_BUF_SIZE;
-	DMA1_Stream5->CR = (4UL << DMA_SxCR_CHSEL_Pos) | DMA_SxCR_CIRC /* Circular mode */
-												   | DMA_SxCR_MINC;
+
+	// LAB manual for Circular RX:
+	// DMA1_Stream5->CR = (4UL << DMA_SxCR_CHSEL_Pos) | DMA_SxCR_CIRC /* Circular mode */
+	//											   | DMA_SxCR_MINC;
+
+	// Code for DMA RX:
+	DMA1_Stream5->CR = (4UL << DMA_SxCR_CHSEL_Pos) |
+	                   DMA_SxCR_MINC |
+	                   DMA_SxCR_TCIE;   // interrupt when 64 bytes received
+
 	DMA1_Stream5->CR |= DMA_SxCR_EN; /* Enable RX DMA */
 
 	NVIC_SetPriority(DMA1_Stream6_IRQn, 1);
 	NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+
+	// RX DMA interrupt
+	NVIC_SetPriority(DMA1_Stream5_IRQn, 1);
+	NVIC_EnableIRQ(DMA1_Stream5_IRQn);
 }
 
 /* TX Transfer Complete ISR */
@@ -117,7 +130,7 @@ void DMA1_Stream6_IRQHandler(void) {
 }
 
 void DMA_UART_Transmit(const char *data, uint16_t len) {
-	while ( DMA1_Stream6->CR & DMA_SxCR_EN); /* wait if busy */
+	while (DMA1_Stream6->CR & DMA_SxCR_EN); /* wait if busy */
 
 	DMA1_Stream6->M0AR = (uint32_t) data;
 	DMA1_Stream6->NDTR = len;
@@ -125,6 +138,25 @@ void DMA_UART_Transmit(const char *data, uint16_t len) {
 	USART2->CR3 |= USART_CR3_DMAT; /* enable UART DMA TX */
 
 	DMA1_Stream6->CR |= DMA_SxCR_EN; /* start transfer */
+}
+
+// RX Receive complete ISR: for DMA RX. Not needed for circular RX
+void DMA1_Stream5_IRQHandler(void)
+{
+    if (DMA1->HISR & DMA_HISR_TCIF5)
+    {
+        DMA1->HIFCR = DMA_HIFCR_CTCIF5;   // clear flag
+
+        /* Echo received 64 bytes */
+        DMA_UART_Transmit((char*)rxBuf, RX_BUF_SIZE);
+
+        /* Restart RX DMA */
+        DMA1_Stream5->CR &= ~DMA_SxCR_EN;
+        while (DMA1_Stream5->CR & DMA_SxCR_EN);
+
+        DMA1_Stream5->NDTR = RX_BUF_SIZE;
+        DMA1_Stream5->CR |= DMA_SxCR_EN;
+    }
 }
 
 void USART2_DMA_Init(void) {
@@ -138,7 +170,7 @@ void USART2_DMA_Init(void) {
 	GPIOA->AFR[0] &= ~((0xFUL << 8) | (0xFUL << 12));
 	GPIOA->AFR[0] |= ((7UL << 8) | (7UL << 12));
 
-	USART2->BRR = (24 << 4) | 7;
+	USART2->BRR = (24 << 4) | 7;   // 0x187
 	USART2->CR3 |= USART_CR3_DMAR; /* enable UART DMA RX */
 	USART2->CR1 = USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
 }
@@ -149,14 +181,15 @@ int main(void) {
 	DMA1_Init();
 	DMA_UART_Transmit(txMsg, strlen(txMsg)); /* non - blocking TX */
 
-	uint16_t prevNDTR = RX_BUF_SIZE;
+	// Commented code are from Lab Manual:
+	// uint16_t prevNDTR = RX_BUF_SIZE;
 	while (1) {
 		/* Detect new RX data by monitoring NDTR decrement */
-		uint16_t curNDTR = DMA1_Stream5->NDTR;
-		if (curNDTR != prevNDTR) {
-			/* New data in rxBuf -- process here */
-			prevNDTR = curNDTR;
-		}
+		// uint16_t curNDTR = DMA1_Stream5->NDTR;
+		// if (curNDTR != prevNDTR) {
+			  /* New data in rxBuf -- process here */
+		//	  prevNDTR = curNDTR;
+		// }
 		__WFI();
 	}
 }
